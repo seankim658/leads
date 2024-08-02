20a61303f7e489a9d7cd68b59406c9bf946fdd99
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-08-02 17:52:23 -0400

    improve title page formatting

--- src/report/pdf.rs
+++ src/report/pdf.rs
@@ -12,11 +12,11 @@ use thiserror::Error;
 /// The default paper size.
 pub const PAPER_SIZE: PdfPagePaperStandardSize = PdfPagePaperStandardSize::A4;
 /// The default font.
-pub const FONT: PdfFontBuiltin = PdfFontBuiltin::Helvetica;
+pub const FONT: PdfFontBuiltin = PdfFontBuiltin::TimesRoman;
 /// The default bold font.
-pub const BOLD_FONT: PdfFontBuiltin = PdfFontBuiltin::HelveticaBold;
+pub const BOLD_FONT: PdfFontBuiltin = PdfFontBuiltin::TimesBold;
 /// The default italic font.
-pub const ITALIC_FONT: PdfFontBuiltin = PdfFontBuiltin::HelveticaOblique;
+pub const ITALIC_FONT: PdfFontBuiltin = PdfFontBuiltin::TimesItalic;
 /// Section header font size.
 pub const SECTION_HEADER_FONT_SIZE: f32 = 24.0;
 /// Normal text font size.
@@ -86,7 +86,11 @@ impl<'a> PageManager<'a> {
     ///
     /// - `data_title`: Title of the dataset.
     /// - `column_types`: Index map of the columns and their corresponding data types.
-    pub fn generate_report(&mut self, data_title: &str, column_types: &IndexMap<String, DataType>) -> Result<(), PdfError> {
+    pub fn generate_report(
+        &mut self,
+        data_title: &str,
+        column_types: &IndexMap<String, DataType>,
+    ) -> Result<(), PdfError> {
         self.create_title_page(data_title)?;
         self.create_data_types_page(column_types)?;
         self.create_table_of_contents()?;
@@ -103,55 +107,35 @@ impl<'a> PageManager<'a> {
     ///
     /// - `Result<(), PdfError>`: Unit type or a propagated PdfError.
     pub fn create_title_page(&mut self, data_title: &str) -> Result<(), PdfError> {
-        let mut page = self
-            .document
-            .pages_mut()
-            .create_page_at_start(PdfPagePaperSize::new_portrait(PAPER_SIZE))?;
+        self.new_page()?;
 
         // Add main document title.
-        let mut title_object = PdfPageTextObject::new(
-            &self.document,
-            "Exploratory Data Analysis Report",
-            self.bold_font,
-            PdfPoints::new(48.0),
-        )?;
-        title_object.set_fill_color(PdfColor::new(0, 0, 0, 255))?;
-        title_object.translate(
-            PdfPoints::new(self.page_width * 0.1),
-            PdfPoints::new(self.page_height * 0.9),
-        )?;
-        page.objects_mut().add_text_object(title_object)?;
+        self.add_text("Exploratory Data", self.bold_font, 48.0, 0.1, 0.9, None)?;
+        self.add_text("Analysis Report", self.bold_font, 48.0, 0.1, 0.83, None)?;
+        //
+        // Add a horizontal line.
+        self.add_line(0.1, 0.80, 0.9, 0.8, 2.0)?;
 
         // Add dataset subtitle.
-        let mut dataset_title_object = PdfPageTextObject::new(
-            &self.document,
+        self.add_text(
             &format!("Dataset: {}", data_title),
             self.font,
-            PdfPoints::new(18.0),
+            18.0,
+            0.1,
+            0.75,
+            None,
         )?;
-        dataset_title_object.set_fill_color(PdfColor::new(0, 0, 0, 255))?;
-        dataset_title_object.translate(
-            PdfPoints::new(self.page_width * 0.1),
-            PdfPoints::new(self.page_height * 0.85),
-        )?;
-        page.objects_mut().add_text_object(dataset_title_object)?;
 
         // Add date.
-        let date = chrono::Local::now().format("%m-%d-%Y").to_string();
-        let mut date_object = PdfPageTextObject::new(
-            &self.document,
-            format!("Generated on: {}", date),
+        let date = chrono::Local::now().format("%B %d, %Y").to_string();
+        self.add_text(
+            &format!("Generated on: {}", date),
             self.font,
-            PdfPoints::new(12.0),
+            12.0,
+            0.1,
+            0.70,
+            None,
         )?;
-        date_object.set_fill_color(PdfColor::new(0, 0, 0, 255))?;
-        date_object.translate(
-            PdfPoints::new(self.page_width * 0.1),
-            PdfPoints::new(self.page_height * 0.75),
-        )?;
-        page.objects_mut().add_text_object(date_object)?;
-
-        self.current_page += 1;
 
         Ok(())
     }
@@ -307,6 +291,24 @@ impl<'a> PageManager<'a> {
             .pages_mut()
             .create_page_at_end(PdfPagePaperSize::new_portrait(PAPER_SIZE))?;
         self.current_page = self.document.pages().len() as u32 - 1;
+        Ok(())
+    }
+
+    // Adds a horizontal line.
+    fn add_line(&mut self, x1: f32, y1: f32, x2: f32, y2: f32, width: f32) -> Result<(), PdfError> {
+        let mut path = PdfPagePathObject::new(
+            &self.document,
+            PdfPoints::new(self.page_width * x1),
+            PdfPoints::new(self.page_height * y1),
+            Some(PdfColor::new(0, 0, 0, 255)),
+            Some(PdfPoints::new(width)),
+            None,
+        )?;
+
+        path.line_to(PdfPoints::new(self.page_width * x2), PdfPoints::new(self.page_height * y2))?;
+
+        let mut current_page = self.document.pages().get(self.current_page as u16).unwrap();
+        current_page.objects_mut().add_path_object(path)?;
         Ok(())
     }
 
