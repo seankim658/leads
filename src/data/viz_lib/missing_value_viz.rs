567e19d67a95764c4dedb9538ca99c21a55e5bce
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-09-20 01:02:32 -0400

    Finish missingness correlation

--- src/data/viz_lib/missing_value_viz.rs
+++ src/data/viz_lib/missing_value_viz.rs
@@ -2,33 +2,50 @@
 //!
 //! This module handles the generation of the visualiations for the missing value analysis.
 
-use super::{SampleModeEnum, SAMPLE_LIMIT, SAMPLE_MODE, SAMPLE_RATIO};
+use super::{
+    create_basic_chart_template, create_drawing_backend, fill_background, LABEL_STYLE,
+    PLOT_CAPTION_FONT, PLOT_HEIGHT, PLOT_MARGIN, PLOT_WIDTH, X_LABEL_AREA_SIZE, Y_LABEL_AREA_SIZE,
+};
 use crate::data::missing_values::MissingValueAnalysis;
 use plotters::prelude::*;
 use polars::{lazy::dsl::*, prelude::*};
+use std::collections::HashMap;
 use std::path::PathBuf;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum MissingValuesPlotError {
-    /// Occurs when sampling the lazy frame fails.
-    #[error("Error sampling the dataframe: {0}")]
-    DataFrameSamplingError(String),
-
     /// Occurs during failure to build the missing matrix.
     #[error("Error building the missing matrix: {0}")]
     BuildMissingMatrixError(String),
+
+    /// Occurs during failure to build the missingness correlation matrix.
+    #[error("Error building the missing matrix: {0}")]
+    BuildMissingnessMatrixError(String),
 
     /// Occurs during failure to draw a chart.
     #[error("Error building the plot: {0}")]
     PlotDrawingError(String),
 }
 
-/// Creates a heatmap visualiation of missing values in the dataset.
+pub fn build_all_visualizations(
+    df: &DataFrame,
+    missing_values_analysis: &MissingValueAnalysis,
+    plot_dir: &PathBuf,
+) -> Result<HashMap<String, PathBuf>, MissingValuesPlotError> {
+    let missing_data_heatmap = build_missing_data_heatmap(df, missing_values_analysis, plot_dir)?;
+    let missingness_correlation_heatmap =
+        build_missingness_correlation_heatmap(df, missing_values_analysis, plot_dir)?;
+    let missing_value_plot_map =
+        HashMap::from([missing_data_heatmap, missingness_correlation_heatmap]);
+    return Ok(missing_value_plot_map);
+}
+
+/// Creates a heatmap visualization of missing values in the dataset.
 ///
 /// ### Parameters
 ///
-/// - `lazy_df`: Reference to the dataset `LazyFrame`.
+/// - `df`: Reference to the dataset `LazyFrame`.
 /// - `missing_values_analysis`: Reference to the `MissingValueAnalysis` struct for the dataset.
 /// - `plot_dir`: Reference to the `PathBuf` where the plot should be saved.
 ///
@@ -36,60 +53,49 @@ pub enum MissingValuesPlotError {
 ///
 /// - `Result<(String, PathBuf), MissingValuesPlotError>`: Result containing a tuple with the plot
 /// title (String) and the output file path (PathBuf), or a `MissingValuesPlotError`.
-pub fn build_missing_data_matrix(
-    lazy_df: &LazyFrame,
+pub fn build_missing_data_heatmap(
+    df: &DataFrame,
     missing_values_analysis: &MissingValueAnalysis,
     plot_dir: &PathBuf,
 ) -> Result<(String, PathBuf), MissingValuesPlotError> {
     let plot_title = "Missing Values Heatmap".to_owned();
 
-    // Sample the data and prepare the matrix.
-    let sampled_df = sample_dataframe(lazy_df)?;
+    // Prepare the matrix.
     let columns: Vec<&str> = missing_values_analysis
         .column_missing_values
         .keys()
         .map(String::as_str)
         .collect();
-    let matrix = build_missing_matrix(&sampled_df, &columns)?;
+    let matrix = build_missing_matrix(df, &columns)?;
 
     let output_path = plot_dir.join("missing_values_heatmap.png");
     // There's probably a better way to do this.
     let output_path_clone = output_path.clone();
 
-    // Create a new drawing backend that is a 1000x600 pixel image.
-    let root = BitMapBackend::new(&output_path_clone, (1200, 800)).into_drawing_area();
-
-    // Fill the background of the plot with white.
-    root.fill(&WHITE.mix(0.95)).map_err(|e| {
-        MissingValuesPlotError::PlotDrawingError(format!(
-            "Error filling the plot background color to white: {}",
-            e
-        ))
-    })?;
-
-    // Split the drawing area into two parts: top for the heatmap, bottom for the legend.
-    // let (top, _bottom) = root.split_vertically(750);
+    let root = create_drawing_backend(&output_path_clone, (PLOT_WIDTH, PLOT_HEIGHT));
+    fill_background(&root, &WHITE, Some(0.95))
+        .map_err(|e| MissingValuesPlotError::PlotDrawingError(e.to_string()))?;
 
     // Create the chart builder for the heatmap.
-    let mut chart = ChartBuilder::on(&root)
-        .caption(&plot_title, ("sans-serif", 35))
-        .margin(5)
-        .x_label_area_size(50)
-        .y_label_area_size(80)
-        .build_cartesian_2d(0..matrix[0].len(), 0..matrix.len())
-        .map_err(|e| {
-            MissingValuesPlotError::PlotDrawingError(format!(
-                "Error building cartesian 2d coordinate system: {}",
-                e
-            ))
-        })?;
-    // Configure the mesh (grid) for the chart.
+    let mut chart = create_basic_chart_template(
+        &root,
+        &plot_title,
+        PLOT_CAPTION_FONT,
+        PLOT_MARGIN,
+        X_LABEL_AREA_SIZE,
+        Y_LABEL_AREA_SIZE,
+        (0..matrix[0].len(), 0..matrix.len()),
+    )
+    .map_err(|e| MissingValuesPlotError::PlotDrawingError(e.to_string()))?;
+
     chart
         .configure_mesh()
         .disable_x_mesh()
         .disable_y_mesh()
         .x_desc("Columns")
+        .x_label_style(LABEL_STYLE)
         .y_desc("Rows")
+        .y_label_style(LABEL_STYLE)
         .x_labels(columns.len())
         .x_label_formatter(&|x| {
             if *x < columns.len() {
@@ -102,7 +108,10 @@ pub fn build_missing_data_matrix(
         .y_label_formatter(&|y| format!("Row {}", y))
         .draw()
         .map_err(|e| {
-            MissingValuesPlotError::PlotDrawingError(format!("Error configuring chart mesh: {}", e))
+            MissingValuesPlotError::PlotDrawingError(format!(
+                "Error configuring chart mesh for missing heatmap: {}",
+                e
+            ))
         })?;
 
     // Draw the heatmap.
@@ -114,99 +123,100 @@ pub fn build_missing_data_matrix(
             })
         }))
         .map_err(|e| {
-            MissingValuesPlotError::PlotDrawingError(format!("Error drawing series on plot: {}", e))
+            MissingValuesPlotError::PlotDrawingError(format!(
+                "Error drawing series on plot for missing heatmap: {}",
+                e
+            ))
         })?;
 
-    // Create the chart builder for the legend.
-    // let mut legend = ChartBuilder::on(&bottom)
-    //     .margin(5)
-    //     .x_label_area_size(30)
-    //     .y_label_area_size(30)
-    //     .build_cartesian_2d(0..1, 0..1)
-    //     .map_err(|e| {
-    //         MissingValuesPlotError::PlotDrawingError(format!("Error building chart legend: {}", e))
-    //     })?;
-    //
-    // // Draw the legend.
-    // legend
-    //     .draw_series(std::iter::once(Rectangle::new(
-    //         [(0, 0), (20, 20)],
-    //         RED.filled(),
-    //     )))
-    //     .map_err(|e| {
-    //         MissingValuesPlotError::PlotDrawingError(format!(
-    //             "Error drawing missing value legend value: {}",
-    //             e
-    //         ))
-    //     })?
-    //     .label("Missing")
-    //     .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], RED));
-    //
-    // // Configure and draw the legend label(s).
-    // legend
-    //     .configure_series_labels()
-    //     .background_style(WHITE.mix(0.8))
-    //     .border_style(BLACK)
-    //     .position(SeriesLabelPosition::UpperRight)
-    //     .draw()
-    //     .map_err(|e| {
-    //         MissingValuesPlotError::PlotDrawingError(format!("Error configuring legend: {}", e))
-    //     })?;
-
-    Ok((plot_title, output_path))
+    Ok((plot_title.clone(), output_path))
 }
 
-/// Collects a lazy frame into a dataframe and applies the sampling if applicable.
-///
-/// Type of sampling depends on the global `SAMPLE_MODE` setting. TODO : Should this eventually be
-/// abstracted into a CLI argument?
-///
-/// ### Parameters
-///
-/// - `lazy_df`: Reference to the dataset `LazyFrame`.
-///
-/// ### Returns
-///
-/// - `Result<DataFrame, MissingValuesPlotError>`: The collected dataframe or the
-/// `MissingValuesPlotError` error.
-fn sample_dataframe(lazy_df: &LazyFrame) -> Result<DataFrame, MissingValuesPlotError> {
-    match SAMPLE_MODE {
-        SampleModeEnum::Limit => {
-            let df = lazy_df
-                .clone()
-                .select([all().sample_n(lit(SAMPLE_LIMIT), false, false, None)])
-                .collect()
-                .map_err(|e| {
-                    MissingValuesPlotError::DataFrameSamplingError(format!(
-                        "Couldn't collect limit ({}) sampled dataframe: {}",
-                        SAMPLE_LIMIT, e
-                    ))
-                })?;
-            Ok(df)
-        }
-        SampleModeEnum::Ratio => {
-            let df = lazy_df
-                .clone()
-                .select([all().sample_frac(lit(SAMPLE_RATIO), false, false, None)])
-                .collect()
-                .map_err(|e| {
-                    MissingValuesPlotError::DataFrameSamplingError(format!(
-                        "Couldn't collect ratio ({}) sampled dataframe: {}",
-                        SAMPLE_RATIO, e
-                    ))
-                })?;
-            Ok(df)
-        }
-        SampleModeEnum::Full => {
-            let df = lazy_df.clone().collect().map_err(|e| {
-                MissingValuesPlotError::DataFrameSamplingError(format!(
-                    "Couldn't collect lazy frame: {}",
-                    e
-                ))
-            })?;
-            Ok(df)
-        }
-    }
+pub fn build_missingness_correlation_heatmap(
+    df: &DataFrame,
+    missing_values_analysis: &MissingValueAnalysis,
+    plot_dir: &PathBuf,
+) -> Result<(String, PathBuf), MissingValuesPlotError> {
+    let plot_title = "Missingness Correlation Heatmap".to_owned();
+
+    // Sample the data and prepare the correlation matrix.
+    let columns: Vec<&str> = missing_values_analysis
+        .column_missing_values
+        .keys()
+        .map(String::as_str)
+        .collect();
+    let correlation_matrix = build_missingness_matrix(df, &columns)?;
+
+    let output_path = plot_dir.join("missingness_correlation_heatmap.png");
+    // There's probably a better way to do this.
+    let output_path_clone = output_path.clone();
+
+    let root = create_drawing_backend(&output_path_clone, (PLOT_WIDTH, PLOT_HEIGHT));
+    fill_background(&root, &WHITE, Some(0.95))
+        .map_err(|e| MissingValuesPlotError::PlotDrawingError(e.to_string()))?;
+
+    let mut chart = create_basic_chart_template(
+        &root,
+        &plot_title,
+        PLOT_CAPTION_FONT,
+        PLOT_MARGIN,
+        X_LABEL_AREA_SIZE,
+        Y_LABEL_AREA_SIZE,
+        (0..columns.len(), 0..columns.len()),
+    )
+    .map_err(|e| MissingValuesPlotError::PlotDrawingError(e.to_string()))?;
+
+    chart
+        .configure_mesh()
+        .disable_x_mesh()
+        .disable_y_mesh()
+        .x_desc("Columns")
+        .x_label_style(LABEL_STYLE)
+        .y_desc("Rows")
+        .y_label_style(LABEL_STYLE)
+        .x_labels(columns.len())
+        .x_label_formatter(&|x| {
+            if *x < columns.len() {
+                columns[*x].to_owned()
+            } else {
+                "".to_owned()
+            }
+        })
+        .y_labels(columns.len())
+        .y_label_formatter(&|y| {
+            if *y < columns.len() {
+                columns[*y].to_owned()
+            } else {
+                "".to_owned()
+            }
+        })
+        .draw()
+        .map_err(|e| {
+            MissingValuesPlotError::PlotDrawingError(format!(
+                "Error configruing chart mesh for missingness correlation heatmap: {}",
+                e
+            ))
+        })?;
+
+    chart
+        .draw_series(correlation_matrix.iter().enumerate().flat_map(|(r, row)| {
+            row.iter().enumerate().map(move |(x, &correlation)| {
+                let color = RGBColor(
+                    (255.0 * (1.0 - correlation.abs())) as u8,
+                    (255.0 * (1.0 - correlation.abs())) as u8,
+                    255,
+                );
+                Rectangle::new([(x, r), (x + 1, r + 1)], color.filled())
+            })
+        }))
+        .map_err(|e| {
+            MissingValuesPlotError::PlotDrawingError(format!(
+                "Error drawing series on plot for missingness correlation heatmap: {}",
+                e
+            ))
+        })?;
+
+    Ok((plot_title.clone(), output_path))
 }
 
 /// Constructs a matrix representing missing values in the dataset.
@@ -249,4 +259,82 @@ fn build_missing_matrix(
         }
     }
     Ok(matrix)
+}
+
+fn build_missingness_matrix(
+    df: &DataFrame,
+    columns: &[&str],
+) -> Result<Vec<Vec<f64>>, MissingValuesPlotError> {
+    let mut correlation_matrix = vec![vec![0.0; columns.len()]; columns.len()];
+
+    for (i, col1) in columns.iter().enumerate() {
+        for (j, col2) in columns.iter().enumerate() {
+            if i == j {
+                correlation_matrix[i][j] = 1.0;
+                continue;
+            }
+
+            let is_missing1: Vec<bool> = df
+                .column(col1)
+                .map_err(|e| {
+                    MissingValuesPlotError::BuildMissingnessMatrixError(format!(
+                        "Error accessing column {}: {}",
+                        col1, e
+                    ))
+                })?
+                .is_null()
+                .into_iter()
+                .map(|opt| opt.unwrap_or(false)) // Convert Option<bool> to bool
+                .collect();
+
+            let is_missing2: Vec<bool> = df
+                .column(col2)
+                .map_err(|e| {
+                    MissingValuesPlotError::BuildMissingnessMatrixError(format!(
+                        "Error accessing column {}: {}",
+                        col1, e
+                    ))
+                })?
+                .is_null()
+                .into_iter()
+                .map(|opt| opt.unwrap_or(false))
+                .collect();
+
+            let correlation = calculate_pearson_coefficient(&is_missing1, &is_missing2);
+            correlation_matrix[i][j] = correlation;
+            correlation_matrix[j][i] = correlation;
+        }
+    }
+
+    Ok(correlation_matrix)
+}
+
+fn calculate_pearson_coefficient(x: &[bool], y: &[bool]) -> f64 {
+    let n = x.len() as f64;
+    let mean_x = x.iter().map(|&b| b as u8 as f64).sum::<f64>() / n;
+    let mean_y = y.iter().map(|&b| b as u8 as f64).sum::<f64>() / n;
+
+    let numerator: f64 = x
+        .iter()
+        .zip(y.iter())
+        .map(|(&a, &b)| ((a as u8 as f64 - mean_x) * (b as u8 as f64 - mean_y)))
+        .sum();
+
+    let denominator_x: f64 = x
+        .iter()
+        .map(|&a| (a as u8 as f64 - mean_x).powi(2))
+        .sum::<f64>()
+        .sqrt();
+
+    let denominator_y: f64 = y
+        .iter()
+        .map(|&b| (b as u8 as f64 - mean_y).powi(2))
+        .sum::<f64>()
+        .sqrt();
+
+    if denominator_x == 0.0 || denominator_y == 0.0 {
+        0.0
+    } else {
+        numerator / (denominator_x * denominator_y)
+    }
 }
